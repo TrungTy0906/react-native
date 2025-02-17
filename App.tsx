280e74ec3f2d69fcca9668819c63694d6f66fa6a
TrungTy09 <votrungty104@gmail.com>
2025-02-17 13:24:16 +0700

    textInput

--- App.tsx
+++ App.tsx
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [count, setCount] = useState(0);
+
+  const [name, setName] = useState<string>("");
+
+  const [age, setAge] = useState<number>(0);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+      <Text>Name:{name}</Text>
+        <TextInput 
+          onChangeText={(value) => setName(value)}
+          style = {{
+              borderColor: 'green',
+              borderWidth: 1,
+              width: 200,
+              padding: 15
+            }} />
+      </View>
+
+      <View>
+      <Text>Age:{age}</Text>
+        <TextInput 
+          onChangeText={(value) => setAge(+value)}
+          keyboardType='number-pad'
+          maxLength={2}
+          style = {{
+              borderColor: 'green',
+              borderWidth: 1,
+              width: 200,
+              padding: 15
+            }} />
+      </View>
     </View>
   );
 }
